71bec1f719719ef860860088484e8b69978f1652
twistofthemyth <twist.of.the.myth@gmail.com>
2024-12-01 00:47:22 +0300

    moved menu rendering impl from Menu.kt to NumericMenuScreen

--- src/main/kotlin/data/Menu.kt
+++ src/main/kotlin/data/Menu.kt
@@ -1,37 +1,6 @@
 package data
 
-import Application
-
-class Menu private constructor(private val header: String, private val actions: List<MenuAction>) {
-
-    private enum class InputStatus {
-        OK,
-        WRONG_FORMAT,
-        NO_HANDLERS
-    }
-
-    fun render() {
-        var terminate = false
-        while (!terminate) {
-            println(header)
-            actions.forEachIndexed { index, action -> println("${index}. ${action.description}") }
-            val input = Application.readUserInput()
-            val status = handleInput(input)
-            when (status) {
-                InputStatus.OK -> terminate = true
-                InputStatus.WRONG_FORMAT -> println("Необходимо ввести цифру")
-                InputStatus.NO_HANDLERS -> println("Такого элемента нет на экране")
-            }
-        }
-    }
-
-    private fun handleInput(input: String): InputStatus {
-        val optionIndex = input.toIntOrNull() ?: return InputStatus.WRONG_FORMAT
-        if (actions.size <= optionIndex) return InputStatus.NO_HANDLERS
-
-        actions[optionIndex].handler.invoke()
-        return InputStatus.OK
-    }
+class Menu private constructor(val header: String, val actions: List<MenuAction>) {
 
     companion object {
         class Builder(private val description: String) {
